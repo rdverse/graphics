a293d1c52b536f2879913c255ed60556c39ba732
redevverse <deveshreddy27@gmail.com>
2021-03-26 09:46:38 -0500

    new project

---
+++ assign2prep.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+
+
+class point{
+
+public:
+    point();
+    float x;
+    float y;
+    float z;
+};
+
+class pointh{
+
+public:
+    pointh();
+    float x;
+    float y;
+    float z;
+};
+
+multiply(point pointReturn, float scalar, point p){
+    pointReturn.x = point.x * scalar;
+    pointReturn.y = point.y * scalar;
+    pointReturn.z = point.z * scalar;
+    return(pointReturn);
+}
+
+
+int main(){
+    // Test 1
+
+    return(0);
+}
---
+++ lame.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+
+using std::cout;
+
+int main(){
+ double xform[3][3];
+
+ xform[0][2] = 20.0;
+ cout<<xform[0][1];
+return 0;
+}
